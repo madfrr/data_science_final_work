feb04fb3f675aed1c610748203ab837e4536cdcd
Gabriel Ferreira <gsferreira.dev@gmail.com>
2025-12-21 12:53:13 -0300

    renamed articles

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-def main():
-    pass
+from src.main import main
 
+def run():
+    main()
+
+if __name__ == "__main__":
+    run()
+
+'''
+- [Done] Extração dos CSVs do drive do malandro (ou do meu drive)
+- Transformação dos CSVs -> selecionar quais colunas necessárias para cada tabela
+- Extração agregada -> jogar todos as colunas que vou precisar + a classificação em um csv/parquet puro
+	- Verificar se vou precisar fazer sampling dos dados por conta de performance
+	- Verificar se vou precisar subir os dados em algum banco de dados
+	- Verificar se vou precisar usar spark
+'''

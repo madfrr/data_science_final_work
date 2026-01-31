a9b2134a68bb49965f46b3a26fd9bea549f8b891
Gabriel Ferreira <gsferreira.dev@gmail.com>
2026-01-31 11:05:42 -0300

    organizing files into folders

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.extract import Extract
 from src.validations import Validations
 from src.metrics import Metrics
-from src.models import gen2out_scores_from_setups, run_callmine_setups
+from src.algorithms import gen2out_scores_from_setups, run_callmine_setups
 from src.extract_selected_columns import Standardization
 from callmine.callmine_focus.run_callmine_focus import runGen2Out
 from callmine.callmine_focus.run_callmine_focus import call_mine_main
@@ -10,13 +10,32 @@ import numpy as np
 import pandas as pd
 from config import Config
 from pathlib import Path
+import os
 
+
+def print_data_types(file_path):
+    if not file_path.exists():
+        print(f"  ✗ Warning: not found {file_path}")
+        return
+
+    print(f"  ✓ Data types:")
+    df = pd.read_csv(file_path)
+    for col in df.columns:
+        print(f"{col}: {df[col].dtype}")
+
+    print("-" * 40)
+    print()
 
 def run_transformation():
     '''
     Aqui com o config_features.csv posso fazer toda análise exploratória
     Para novos conjuntos de features para rodar os modelos, posso exportar csvs diferentes... (config_features_1.csv, config_features_2.csv, etc)
     '''
+    path = Config.data_path / "configs"
+    if os.path.exists(path) and os.listdir(path):
+        print(f"\n✓ Folder '{path}' already exists and is not empty. Skipping download.")
+        return
+    
     CreateConfigs(data_path=Config.data_path, input_dir="data_selected_columns", output_dir="configs").run()
     FeatureEngineering().base_config()
     FeatureEngineering().generate_first_config()
@@ -53,13 +72,15 @@ def main(config: Config):
     run_transformation()
     # -- análise exploratória tem que ser aqui
     
-    run_callmine_setups(Config.CallMine.setups)
+
+
+    # run_callmine_setups(Config.CallMine.setups)
     
-    scores_sorted = gen2out_scores_from_setups(read_if_cached=False, is_to_save=False, inverted=True)
-    address_to_class = get_address_class_lookup()
-    print()
-    for setup, scores in scores_sorted.items():
-        print('='*40)
-        print(f"Running metrics for {setup} scores")
-        print('='*40)
-        run_metrics(setup, scores, address_to_class)
+    # scores_sorted = gen2out_scores_from_setups(read_if_cached=False, is_to_save=False, inverted=True)
+    # address_to_class = get_address_class_lookup()
+    # print()
+    # for setup, scores in scores_sorted.items():
+    #     print('='*40)
+    #     print(f"Running metrics for {setup} scores")
+    #     print('='*40)
+    #     run_metrics(setup, scores, address_to_class)
